2ec2000fab51e18677c572c31f711976e6dd7bb4
Lenin Luque <xleninx@gmail.com>
2017-05-05 18:14:00 -0300

    create routes of endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'show', to: 'waypoints#index'
+
+  namespace :api do
+    namespace :v1, constraints: { format: 'json' }  do
+      post 'gps', to: 'waypoints#create'
+    end
+  end
 end
