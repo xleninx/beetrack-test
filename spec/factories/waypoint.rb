71535e18a982f8b97f47c0b6712601eb949976e4
Lenin Luque <xleninx@gmail.com>
2017-05-05 19:12:36 -0300

    add sent at to factory

--- spec/factories/waypoint.rb
+++ spec/factories/waypoint.rb
@@ -3,5 +3,6 @@ FactoryGirl.define do
     latitude      { Faker::Address.latitude }
     longitude      { Faker::Address.longitude }
     vehicle        { build(:vehicle) }
+    sent_at        { Time.now }
   end
 end
